d2c2dc1970454f3afa16f0f5f042c1ca5db6c64c
igor udot <horw7001@gmail.com>
2023-10-30 20:23:39 +0300

    test: add tests

--- pypipe.py
+++ pypipe.py
@@ -16,10 +16,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import sys
 import argparse
+import sys
 from os import chmod
-
 
 __version__ = "0.2.0"
 
--- tests/test_pypipe_examples.py
+++ tests/test_pypipe_examples.py
@@ -1,3 +1,4 @@
+import io
 import sys
 from pathlib import Path
 
@@ -14,14 +15,41 @@ TEST_DATA_DIR = Path(__file__).resolve().parent / 'data'
     ('staff.txt', 'ppp_rec_1.txt', ['rec', 'r[:3]']),
     ('staff.txt', 'ppp_rec_2.txt', ['rec', '-l5', 'f"{f1} is {f4} years old"']),
     ('staff.txt', 'ppp_rec_3.txt', ['rec', '-H', 'rec[0], dic["Birth"]']),
+    ('echo_rec_1.txt', 'ppp_rec_4.txt', ['rec', '-l5', '-t', '2:i,3:f,4:b,5:j',
+                                         "type(f1),type(f2),type(f3),type(f4),type(f5)"]),
+    ('staff.csv', 'ppp_rec_5.txt', ['rec', '-d', ',', '-l6', 'f1']),
+    ('echo_rec_2.txt', 'ppp_rec_6.txt', ['rec', '-d', r'\s+', 'rec[2]']),
+    ('staff.txt', 'ppp_rec_7.txt', ['rec', '-D' ',']),
+    ('echo_rec_3.txt', 'ppp_rec_8.txt', ['rec', '-m', r'\d+', 'r[1]']),
+    ('staff.csv', 'ppp_csv_1.txt', ['csv', '-O', 'quoting=csv.QUOTE_ALL']),
+    ('staff.txt', 'ppp_text_1.txt', ['text', "len(text)"]),
+    ('staff.json', 'ppp_text_2.txt', ['text', '-j', 'dic["data"][0]']),
 ])
-def test_ppp_line(input_text_file_name, expected_text_file_name, command,capsys):
+def test_ppp_common(input_text_file_name, expected_text_file_name, command, capsys):
     ex_data: str
-    with open(TEST_DATA_DIR / 'input' / input_text_file_name) as file:
-        sys.stdin = (_l for _l in file.readlines())
-    with open(TEST_DATA_DIR / 'expect' / expected_text_file_name) as file:
-        ex_data = file.read()
+    try:
+        sys.stdin = open(TEST_DATA_DIR / 'input' / input_text_file_name)
+        with open(TEST_DATA_DIR / 'expect' / expected_text_file_name) as file:
+            ex_data = file.read()
 
-    main(command)
+        main(command)
+        out, err = capsys.readouterr()
+        assert out.replace("\r\n", "\n") == ex_data.replace("\r\n", "\n")
+    finally:
+        sys.stdin.close()
+
+
+def test_ppp_file(capsys):
+    f = io.StringIO()
+    f.write(str(TEST_DATA_DIR / 'input' / 'staff.json')+"\n")
+    f.write(str(TEST_DATA_DIR / 'input' / 'staff.txt')+"\n")
+    f.seek(0)
+    sys.stdin = f
+
+    main(['file', 'path, len(text)'])
     out, err = capsys.readouterr()
-    assert out == ex_data
+    expect = [
+        str(TEST_DATA_DIR / 'input' / 'staff.json') + "\t" + "1046\n",
+        str(TEST_DATA_DIR / 'input' / 'staff.txt') + "\t" + "231\n"
+    ]
+    assert out == ''.join(expect)
